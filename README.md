e563306fe926abaa37012e715cddb48f6f4d3ef2
ArsFy <arsfy@outlook.com>
2023-03-12 10:56:33 +0800

    add edit node

--- README.md
+++ README.md
@@ -9,6 +9,10 @@
 - 傳送 `/now` 命令，推送選定 Server ID 的伺服器流量使用情況/Uptime
 - 每天 23:55 推送今天流量使用情況
 
+- `/nodelist` 檢視當前記錄的 NodeID
+- `/addnode` 新增 NodeID （需要配置 admin user id）
+- `/delnode` 刪除 NodeID （需要配置 admin user id）
+
 ## Start
 
 ```
@@ -34,6 +38,7 @@ pip3 install matplotlib
     "nezha": "wss://xx.xxx.com/ws",        // 使用探針網域替換 xx.xxx.com
     "group": "@xxx",                       // 日報推送群組
     "nodelist": [1, 2]                     // NodeID，nezha panel 有顯示
+    "admin": [ 100000000 ]             // Telegram Admin User ID
 }
 ```
 
--- main.js
+++ main.js
@@ -111,6 +111,61 @@ bot.on("message", async (msg) => {
                     })
                 });
                 break;
+            case "/month": case "/month" + config.bot:
+                let m1 = {}
+                for (let i = 0; i < nodedata.length; i++) {
+                    if (nodedata[i].time > new Date() - 30 * 24 * 60 * 60 * 1000) {
+                        m1 = nodedata[i];
+                        break;
+                    }
+                }
+                let now = nodedata[nodedata.length - 1], text1 = "";
+
+                for (let i = 0; i < now.data.length; i++) {
+                    for (let ii = 0; ii < m1.data.length; ii++) {
+                        if (now.data[i].name == m1.data[ii].name) {
+                            let out_t = (now.data[i].out - m1.data[ii].out) / (1024 ** 3);
+                            let in_t = (now.data[i].in - m1.data[ii].in) / (1024 ** 3);
+                            text1 += `# ${now.data[i].name}\nInput: ${(in_t).toFixed(2)}GB\nOutput: ${(out_t).toFixed(2)}GB\nAllTransfer: ${(out_t + in_t).toFixed(2)}GB`
+                            if (i != now.data.length - 1) {
+                                text1 += "\n\n"
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                bot.sendMessage(chatId, `${moment(new Date(m1.time)).format("YYYY-MM-DD HH:mm")} -> ${moment(new Date(now.time)).format("YYYY-MM-DD HH:mm")}\n\n${text1}`)
+                break;
+            case "/nodelist": case "/nodelist" + config.bot:
+                bot.sendMessage(chatId, `設定中包含的NodeID:\n${config.nodelist.join(", ")}`)
+                break
+            case "/addnode": case "/addnode" + config.bot:
+                if (config.admin.indexOf(msg.from.id) != -1) {
+                    if (command.length == 2 && config.nodelist.indexOf(Number(command[1])) == -1) {
+                        config.nodelist.push(Number(command[1]));
+                        fs.writeFileSync("./config.json", JSON.stringify(config, null, "  "));
+                        bot.sendMessage(chatId, `新增 NodeID: ${Number(command[1])}`)
+                    } else {
+                        bot.sendMessage(chatId, `發生問題，缺少參數/ID已存在: /addnode id`)
+                    }
+                } else {
+                    bot.sendMessage(chatId, `只有 bot admin 有權限執行這個操作`)
+                }
+                break
+            case "/delnode": case "/delnode" + config.bot:
+                if (config.admin.indexOf(msg.from.id) != -1) {
+                    if (command.length == 2 && config.nodelist.indexOf(Number(command[1])) != -1) {
+                        config.nodelist.splice(config.nodelist.indexOf(Number(command[1])), 1)
+                        fs.writeFileSync("./config.json", JSON.stringify(config, null, "  "));
+                        bot.sendMessage(chatId, `刪除 NodeID: ${Number(command[1])}`)
+                    } else {
+                        bot.sendMessage(chatId, `發生問題:，缺少參數: /delnode id`)
+                    }
+                } else {
+                    bot.sendMessage(chatId, `只有 bot admin 有權限執行這個操作`)
+                }
+                break
         }
     }
 });
